97679205d2fb76f728fb6cfbd88a0443eb46c192
Shashank Arya <shashankarya9831@gmail.com>
2024-04-06 14:43:41 +0530

    Add files via upload

---
+++ 01-Basics/threenumlargest.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    if( a>b && a>c )
+    {
+        printf("The greatest number is: %d\n",a);
+    }
+
+    else if( b>a && b>c )
+    {
+        printf("The greatest number is: %d\n",b);
+    }
+
+    else if( c>a && c>a )
+    {
+        printf("The greatest number is: %d\n",c);
+    }
+
+    return 0;
+}
