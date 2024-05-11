fd6e9c23c09ffb45d3382a26608590e6d89bc6ae
Shashank Arya <shashankarya9831@gmail.com>
2024-05-11 23:28:00 +0530

    Update 2Dvector.cpp

--- 02-Arrays-and-Vectors/2Dvector.cpp
+++ 02-Arrays-and-Vectors/2Dvector.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
 	// a 2D vector is a vector of vectors, so intuitively the following syntax comes in mind.
-	
 	vector<vector<int>> movie_ratings
 	{
 		{1,2,3,4},
@@ -14,16 +14,13 @@ int main()
 	};
 
 	// rows: reviewers, columns: movies
-
 	cout << "\nMovie rating for reviewer 1 using array syntax: " << endl;
-
 	cout << movie_ratings[0][0] << endl;
 	cout << movie_ratings[0][1] << endl;
 	cout << movie_ratings[0][2] << endl;
 	cout << movie_ratings[0][3] << endl;
 
         cout << "\nMovie rating for reviewer 1 using vector syntax: " << endl;	
-
 	cout << movie_ratings.at(0).at(0) << endl;
 	cout << movie_ratings.at(0).at(1) << endl;
 	cout << movie_ratings.at(0).at(2) << endl;
