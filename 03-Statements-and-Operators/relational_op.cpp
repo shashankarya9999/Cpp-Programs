8cdfecddf97de609307082234e563bee023b94ff
Shashank Arya <shashankarya9831@gmail.com>
2024-05-12 20:33:39 +0530

    Update relational_op.cpp

--- 03-Statements-and-Operators/relational_op.cpp
+++ 03-Statements-and-Operators/relational_op.cpp
@@ -11,10 +11,10 @@ int main()
     cout << "Enter 2 integers seperated by space: ";
     cin >> num1 >> num2;
 
-    cout << num1 << " > " << num2 << " : " << (num1 > num2) << endl;
-    cout << num1 << " >= " << num2 << " : " << (num1 >= num2) << endl;
-    cout << num1 << " < " << num2 << " : " << (num1 < num2) << endl;
-    cout << num1 << " <= " << num2 << " : " << (num1 <= num2) << endl;
+    cout << num1 << " > " << num2 << " : " << ( num1 > num2 ) << endl;
+    cout << num1 << " >= " << num2 << " : " << ( num1 >= num2 ) << endl;
+    cout << num1 << " < " << num2 << " : " << ( num1 < num2 ) << endl;
+    cout << num1 << " <= " << num2 << " : " << ( num1 <= num2 ) << endl;
 
     return 0;
 }
