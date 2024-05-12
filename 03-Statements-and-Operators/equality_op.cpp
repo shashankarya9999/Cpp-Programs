2dae7769234da7553704d58a7d35e7bcd82df750
Shashank Arya <shashankarya9831@gmail.com>
2024-05-13 00:06:43 +0530

    Update equality_op.cpp

--- 03-Statements-and-Operators/equality_op.cpp
+++ 03-Statements-and-Operators/equality_op.cpp
@@ -14,8 +14,8 @@ int main()
     cout << "Enter two integers seperated by space: ";
     cin >> num1 >> num2;
 
-    equal_result = (num1 == num2);
-    not_equal_result = (num1 != num2);
+    equal_result = ( num1 == num2 );
+    not_equal_result = ( num1 != num2 );
 
     cout << "Comparison result(equals): " << equal_result << endl;
     cout << "Comparison result(not equals): " << not_equal_result << endl;
