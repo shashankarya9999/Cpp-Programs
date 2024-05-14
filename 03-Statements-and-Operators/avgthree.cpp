1268ff0d297548757ee0cfe657ba52c9016f523f
Shashank Arya <shashankarya9831@gmail.com>
2024-05-15 01:21:41 +0530

    Update avgthree.cpp

--- 03-Statements-and-Operators/avgthree.cpp
+++ 03-Statements-and-Operators/avgthree.cpp
@@ -30,7 +30,9 @@ int main()
     average = static_cast<double>(total) / count;
 
     cout << "The 3 numbers were: " << num1 << ", " << num2 << ", " << num3 << endl;
+
     cout << "The sum of the numbers is: " << total << endl;
+
     cout << "The average of the numbers is: " << average << endl;
 
     return 0; 
