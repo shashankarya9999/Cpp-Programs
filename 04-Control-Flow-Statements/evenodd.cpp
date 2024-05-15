f9e0769e87ac3c2aab3fae0919a3d51cf201ddb3
Shashank Arya <shashankarya9831@gmail.com>
2024-05-16 01:30:48 +0530

    Add files via upload

---
+++ 04-Control-Flow-Statements/evenodd.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num {};
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    if( num % 2 == 0 )
+    {
+        cout << num << " is even " << endl;
+    }
+
+    else
+    {
+        cout << num << " is odd " << endl;
+    }
+
+    cout << num << " is " << (( num % 2 == 0 ) ? "even" : "odd") << endl;
+
+    return 0;
+}
