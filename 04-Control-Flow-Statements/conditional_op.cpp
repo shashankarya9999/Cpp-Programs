e2680eb12ee084d327528d359fcc777176eab8aa
Shashank Arya <shashankarya9831@gmail.com>
2024-05-16 01:30:28 +0530

    Add files via upload

---
+++ 04-Control-Flow-Statements/conditional_op.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    
+    // Conditional Operator Examples
+
+    int a {10}, b {20};
+    int score {92};
+    int result {};
+
+    result = (a > b) ? a : b;
+
+    result = (a < b) ? (b - a) : (a - b);
+
+    result = (b != 0) ? (a / b) : 0;
+
+    cout << ((score > 90) ? "Excellent" : "Good");
+    
+    */
+
+    return 0;
+}
