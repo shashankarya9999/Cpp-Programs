af56f774c2753ebfaefa6dc69c7a55b997e1c7f5
Shashank Arya <shashankarya9831@gmail.com>
2024-05-17 00:46:15 +0530

    Update find_string.cpp

--- 05-Characters-and-Strings/find_string.cpp
+++ 05-Characters-and-Strings/find_string.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main()
 {
     // string s1 {"This is a test"};
-
     // cout << s1.find("This") << endl;    // 0
     // cout << s1.find("is") << endl;      // 2
     // cout << s1.find("test") << endl;    // 10
