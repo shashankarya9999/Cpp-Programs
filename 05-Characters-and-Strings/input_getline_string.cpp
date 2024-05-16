97c16fa8883f7b5c0b4345f4c408703c2b4b9c92
Shashank Arya <shashankarya9831@gmail.com>
2024-05-17 00:48:10 +0530

    Update input_getline_string.cpp

--- 05-Characters-and-Strings/input_getline_string.cpp
+++ 05-Characters-and-Strings/input_getline_string.cpp
@@ -7,15 +7,15 @@ int main()
 {
     // string s1;
     // cout << "Enter a string: ";
-    // cin >> s1;          // Hello there
-    //                     // only accepts up to the first space
-    // cout << s1 << endl; // Hello
+    // cin >> s1;                  // Hello there
+    //                             // only accepts up to the first space
+    // cout << s1 << endl;         // Hello
 
-    // getline(cin, s1);   // Read entire line until \n
-    // cout << s1 << endl; // Hello there
+    // getline(cin, s1);           // Read entire line until \n
+    // cout << s1 << endl;         // Hello there
 
-    // getline(cin, s1, 'x');  // this isx
-    // cout << s1 << endl;     // this is
+    // getline(cin, s1, 'x');      // this isx
+    // cout << s1 << endl;         // this is
 
     string full_name {};
 
