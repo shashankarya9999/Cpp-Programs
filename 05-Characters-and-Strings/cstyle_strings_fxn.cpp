b51e6a67c71a5fd1906b3f0af1d33f06727ce72f
Shashank Arya <shashankarya9831@gmail.com>
2024-05-17 00:45:11 +0530

    Update cstyle_strings_fxn.cpp

--- 05-Characters-and-Strings/cstyle_strings_fxn.cpp
+++ 05-Characters-and-Strings/cstyle_strings_fxn.cpp
@@ -4,17 +4,15 @@
 int main()
 {
     /*
-
-    char str[80];
-
-    strcpy(str, "Hello ");      // copy
-
-    strcat(str, "there");       // concatenate
-
-    cout << strlen(str);        // 11
-
-    strcmp(str, "Another");     // > 0
-
+        char str[80];
+    
+        strcpy(str, "Hello ");      // copy
+    
+        strcat(str, "there");       // concatenate
+    
+        cout << strlen(str);        // 11
+    
+        strcmp(str, "Another");     // > 0
     */
 
     return 0;
