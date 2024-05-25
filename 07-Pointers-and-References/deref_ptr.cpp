2b270542a421dbc4166dce8f185b08dec1cd02f4
Shashank <shashankarya9831@gmail.com>
2024-05-25 12:04:37 +0530

    Add files via upload

--- 07-Pointers-and-References/deref_ptr.cpp
+++ 07-Pointers-and-References/deref_ptr.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include<vector>
+#include<string>
 
 using namespace std;
 
 int main()
 {
+    // int
+
     int score {100};
     int *score_ptr {&score};
 
@@ -13,6 +17,39 @@ int main()
 
     cout << *score_ptr << endl;
     cout << score << endl;
+
+    cout << endl;
+
+    // string
+
+    string name {"Shashank"};
+
+    string *string_ptr {&name};
+
+    cout << *string_ptr << endl;
+
+    name = "Arya";
+
+    cout << *string_ptr << endl;
+
+    cout << endl;
+
+    // vector
+
+    vector<string> stooges {"Larry","Moe","Curly"};
+    vector<string> *vector_ptr {nullptr};
+
+    vector_ptr = &stooges;
+
+    cout << "First stooge: " << (*vector_ptr).at(0) << endl;    // Larry
+    // () before . operator because . has higher priority than *
+
+    cout << "Stooges: ";
+
+    for(auto stooge: *vector_ptr)
+        cout << stooge << " ";
+    
+    cout << endl;
 
     return 0;
 }
