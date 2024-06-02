dbffaf504cef388c8dcf47afc03ea29cccafec90
Shashank <shashankarya9831@gmail.com>
2024-06-02 20:34:30 +0530

    Add files via upload

---
+++ 08-Classes-and-Objects/const_with_constructor.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Player
+{
+    private:
+        std::string name;
+        int health;
+        int xp;
+
+    public:
+        std::string get_name() const
+        {
+            return name;
+        }
+
+        void set_name(std::string name_val)
+        {
+            name = name_val;
+        }
+
+        // overloaded constructor
+        Player();
+        Player(std::string name_val);
+        Player(std::string name_val, int health_val, int xp_val);
+};
+
+Player::Player()
+    :Player {"None",0,0} {}
+
+Player::Player(std::string name_val)
+    :Player {name_val,0,0} {}
+
+Player::Player(std::string name_val, int health_val, int xp_val)
+    :name {name_val}, health {health_val}, xp {xp_val} {}
+
+void display_player_name(const Player &p)
+{
+    cout << p.get_name() << endl;
+}
+
+int main()
+{
+    const Player villain {"Villain",100,55};
+    Player hero {"Hero",100,15};
+
+    // villain.set_name("Super Villain");
+    // villain.get_name() << endl;
+
+    cout << hero.get_name() << endl;
+
+    display_player_name(villain);
+    display_player_name(hero);
+
+    return 0;
+}
