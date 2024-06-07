b9c0c406d20acc470bdcbf06a49440dfd062ad95
Shashank <shashankarya9831@gmail.com>
2024-06-08 05:10:29 +0530

    Add files via upload

---
+++ 11-Polymorphism/final.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+    class A
+    {
+        public:
+            virtual void do_something();
+    };
+
+    class B: public A
+    {
+        virtual void do_something() final;  // prevents further overriding
+    };
+
+    class C: public B
+    {
+        virtual void do_something();        // compiler error - can't override
+    };
+*/
+
+int main()
+{
+    return 0;
+}
