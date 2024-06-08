8e928fe30fe92eef0de42651e8de8e277c621779
Shashank <shashankarya9831@gmail.com>
2024-06-08 19:56:47 +0530

    Add files via upload

---
+++ 12-Smart-Pointers/init_smartptr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+    // basic syntax of smart pointers
+    // std::smart_pointer<Some_Class> ptr = ...
+
+    // ptr -> method();
+    // std::cout << (*ptr) << endl;
+}
+
+// ptr will be destroyed automatically 
+// when no longer needed
