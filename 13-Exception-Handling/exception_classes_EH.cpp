aed832a4dcb8b5550a86a6250fb68948205c8760
Shashank <shashankarya9831@gmail.com>
2024-06-09 11:16:38 +0530

    Add files via upload

---
+++ 13-Exception-Handling/exception_classes_EH.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+class DivideByZeroException
+{
+    
+};
+
+class NegativeValueException
+{
+
+};
+
+double calculate_mpg(int miles, int gallons)
+{
+    if(gallons == 0)
+        throw DivideByZeroException();
+
+    if(miles < 0 || gallons < 0)
+        throw NegativeValueException();
+
+    return static_cast<double>(miles) / gallons;
+}
+
+int main()
+{
+    int miles {};
+    int gallons {};
+    double miles_per_gallon {};
+
+    std::cout << "Enter the miles: ";
+    std::cin >> miles;
+    std::cout << "Enter the gallons: ";
+    std::cin >> gallons;
+
+    try
+    {
+        miles_per_gallon = calculate_mpg(miles,gallons);
+        std::cout << "Result: " << miles_per_gallon << std::endl;
+    }   
+
+    catch(int &ex)
+    {
+        std::cerr << "Sorry, can't divide by zero" << std::endl;
+    }   
+
+    catch(std::string &ex)
+    {
+        std::cerr << ex << std::endl;
+    }
+
+    // catch(...) {}
+
+    std::cout << "Bye" << std::endl;
+
+    return 0;    
+}
--- 13-Exception-Handling/stack_unwinding.cpp
+++ 13-Exception-Handling/stack_unwinding.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 
+void func_a();
+void func_b();
+void func_c();
+
 void func_a()
 {
     std::cout << "Starting func_a" << std::endl;
@@ -10,13 +14,25 @@ void func_a()
 void func_b()
 {
     std::cout << "Starting func_b" << std::endl;
-    func_c();
+    try
+    {
+        func_c();
+    }
+
+    catch(int &ex)
+    {
+        std::cout << "Caught error in func_b" << std::endl;
+    }
+
     std::cout << "Ending func_b" << std::endl;
 }
 
 void func_c()
 {
     std::cout << "Starting func_c" << std::endl;
+
+    throw 100;
+    
     std::cout << "Ending func_c" << std::endl;
 }
 
@@ -24,9 +40,17 @@ int main()
 {
     std::cout << "Starting main" << std::endl;
 
-    func_a();
+    try
+    {
+        func_a();
+    }
+
+    catch(int &ex)
+    {
+        std::cerr << "Caught error in main" << std::endl;
+    }
 
     std::cout << "Finishing main" << std::endl;
-
+    
     return 0;
 }
