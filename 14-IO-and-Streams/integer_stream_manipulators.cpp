b1043f99d4694920f601b1bc26eba10908820e37
Shashank <shashankarya9831@gmail.com>
2024-06-09 22:45:57 +0530

    modified integer_stream_manipulators.cpp

--- 14-IO-and-Streams/integer_stream_manipulators.cpp
+++ 14-IO-and-Streams/integer_stream_manipulators.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <iomanip>
 
-int main() {
+int main() 
+{
     int num {255};
  
-    // Displaying using different bases
+    // displaying using different bases
     std::cout << "\n------------------------------------------------------------" << std::endl;
     std::cout << std::dec << num << std::endl;
     std::cout << std::hex << num << std::endl;
     std::cout << std::oct << num << std::endl;
     
-    // Displaying showing the base prefix for hex and oct
+    // displaying showing the base prefix for hex and oct
         std::cout << "\n------------------------------------------------------------" << std::endl;
     std::cout << std::showbase;
     std::cout << std::dec << num << std::endl;
     std::cout << std::hex << num << std::endl;
     std::cout << std::oct << num << std::endl;
     
-     // Display the hex value in uppercase
+     // display the hex value in uppercase
     std::cout << "\n------------------------------------------------------------" << std::endl;
     std::cout << std::showbase << std::uppercase;
     std::cout << std::hex << num << std::endl;
 
-    // Display the + sign in front of positive base 10 numbers
+    // display the + sign in front of positive base 10 numbers
     std::cout << "\n------------------------------------------------------------" << std::endl;
     std::cout << std::showpos;
     std::cout << std::dec << num << std::endl;
@@ -55,7 +56,8 @@ int main() {
     std::cout << "Hexadecimal      : " << std::hex << std::showbase << num << std::endl;
     std::cout << "Octal            : " << std::oct << num << std::endl;
     
-    std::cout << std::endl << std::endl;
+    std::cout << std::endl;
+    
     return 0;
 }
 
