77faebba25fcd85fae769381356e412bb466fd72
Shashank <shashankarya9831@gmail.com>
2024-06-11 01:19:00 +0530

    Add files via upload

---
+++ 14-IO-and-Streams/Romeo-Juliet/README.md
@@ -0,0 +1,7 @@
+### Copy Romeo and Juliet with line numbers
+
+Provided the text to Romeo and Juliet in a file named romeojuliet.txt, I have to make a copy of the Romeo and Juliet file and save it to another file. <br>
+
+This new copy should have line numbers at the beginning of each line in the play. Please format the numbers nicely so everything lines up.
+
+
---
+++ 14-IO-and-Streams/Romeo-Juliet/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<string>
+
+int main()
+{
+    std::fstream in_file {"romeojuliet.txt"};
+    std::ofstream out_file {"romeojuliet_out.txt"};
+
+    if(!in_file.is_open())
+    {
+        std::cerr << "Error opening input file" << std::endl;
+
+        return 1;
+    }
+
+    if(!out_file.is_open())
+    {
+        std::cerr << "Error opening input file" << std::endl;
+
+        return 1;
+    }
+
+    std::string line {};
+    int line_number {0};
+
+    while(getline(in_file, line))
+    {
+        // put line number on the lines which contain actual text only
+        if(line == "" || line == "\r")
+            out_file << std::endl;
+        
+        else
+        {            
+            ++line_number;
+
+            out_file << std::setw(7) << std::left << line_number
+                     << line << std::endl;
+        }
+    }
+
+    std::cout << "\nCopy Complete" << std::endl;
+
+    in_file.close();
+    out_file.close();
+
+    return 0;
+}
+
