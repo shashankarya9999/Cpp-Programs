082bd4d9dd471055d112fa60e7e735da8f8f97fb
Shashank <shashankarya9831@gmail.com>
2024-06-10 05:58:48 +0530

    Add files via upload

---
+++ 14-IO-and-Streams/Read-File-1/main.cpp
@@ -0,0 +1,30 @@
+// Test for file open and simple read of 3 data items
+
+#include<iostream>
+#include<fstream>
+
+int main()
+{
+    std::ifstream in_file;
+    in_file.open("test.txt");
+
+    std::string line;
+    int num;
+    double total;
+    
+    if(!in_file)
+    {
+        std::cerr << "Problem opening file" << std::endl;
+        
+        return 1;
+    }
+
+    in_file >> line >> num >> total;
+    std::cout << line << std::endl;
+    std::cout << num << std::endl;
+    std::cout << total << std::endl;
+    
+    in_file.close();
+
+    return 0;
+}
---
+++ 14-IO-and-Streams/Read-File-2/main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+
+int main()
+{
+    std::ifstream in_file;
+    std::string line;
+    int num;
+    double total;
+
+    in_file.open("test.txt");
+
+    if(!in_file)
+    {
+        std::cerr << "Problem opening file" << std::endl;
+        
+        return 1;
+    }
+
+    while(!in_file.eof())
+    {
+        in_file >> line >> num >> total;
+
+        std::cout << std::setw(10) << std::left << line
+                << std::setw(10) << num
+                << std::setw(10) << total
+                << std::endl;
+    }
+
+    // another way
+    /*
+        while(in_file >> line >> num >> total)
+        {
+            std::cout << std::setw(10) << std::left << line
+                << std::setw(10) << num
+                << std::setw(10) << total
+                << std::endl;
+        }
+    */
+
+    in_file.close();
+
+    return 0;
+}
